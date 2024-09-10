044af3941ff29441575a395fb9a56beb3f25d3e1
samitcapcons <samit@capcons.com>
2024-09-10 15:11:07 +0530

    updated code for telegram bot

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+require('dotenv').config();
+const depositRoutes = require('./routes/depositRoutes');
+
+const app = express();
+app.use(express.json());
+
+//connect db
+require("./config/db").connect();
+
+//routes
+app.use('/api/v1', depositRoutes);
+app.use('/',(req,res)=>{
+res.send('ETH DEPOSIT TRACKER ON SEPOLIA')
+})  
+
+//connect to the port and start server
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

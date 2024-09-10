bf1be3d875b0db69816f990a7d863a220b409692
samitcapcons <samit@capcons.com>
2024-09-10 16:21:05 +0530

    error resolved!!

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-const depositRoutes = require('./routes/depositRoutes');
+const depositRoutes = require('./routes/depositTransactionRoute');
 
 const app = express();
 app.use(express.json());
